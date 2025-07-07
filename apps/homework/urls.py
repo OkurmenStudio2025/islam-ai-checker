f9ba180ab6671d173d6ddb885ef1be83b39b43e1
OkurmenStudio2025 <okurmenstudio2025@gmail.com>
2025-07-07 14:50:47 +0600

    Update urls.py

--- apps/homework/urls.py
+++ apps/homework/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import HomeworkReviewCreateAPIView
 
 urlpatterns = [
-    path('homework/', HomeworkReviewCreateAPIView.as_view(), name='homework'),
+    path('homework/', HomeworkReviewCreateAPIView.as_view()),        # POST
+    path('homework/<int:pk>/', HomeworkReviewCreateAPIView.as_view()),  # GET by ID
 ]
