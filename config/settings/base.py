641bc8bc44e1086688ae5eb7a52dfd9f3f8bd4bc
OkurmenStudio2025 <okurmenstudio2025@gmail.com>
2025-07-07 14:36:15 +0600

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+     'drf_spectacular',
+    'drf_spectacular_sidecar',
     'apps.group',
     'apps.homework'
 ]
