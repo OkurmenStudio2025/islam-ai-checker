aa63f9a3265b941b4833c157769fcacd73bacaba
OkurmenStudio2025 <okurmenstudio2025@gmail.com>
2025-07-07 15:14:02 +0600

    Update urls.py

--- apps/homework/urls.py
+++ apps/homework/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import HomeworkViewSet, HomeworkReviewCreateAPIView
 from django.urls import path, include
 
+
 router = DefaultRouter()
 router.register(r'homework-results', HomeworkViewSet, basename='homework-result')
 
